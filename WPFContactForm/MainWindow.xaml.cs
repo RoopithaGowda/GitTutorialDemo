7f877c554310b1f686f1b57a7a04a7527be388b2
Roopitha <roopitha.n@gmail.com>
2023-01-23 14:58:26 +0530

    change

--- WPFContactForm/MainWindow.xaml.cs
+++ WPFContactForm/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace WPFContactForm
 {
     /// <summary>
-    /// Interaction logics to MainWindow.xaml
+    /// Interaction logic to MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
